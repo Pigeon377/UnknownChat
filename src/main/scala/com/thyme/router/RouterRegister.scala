88d8126422782baf0265aec44b5d205bab122288
pigeon_gugugu <892197240@qq.com>
2021-12-30 20:44:20 +0800

    feat: fix directory

---
+++ src/main/scala/com/thyme/router/RouterRegister.scala
@@ -0,0 +1,15 @@
+package com.thyme.router
+
+import akka.actor.ActorSystem
+
+import scala.concurrent.ExecutionContext
+
+object RouterRegister {
+    def registerRouter(implicit ex: ExecutionContext, system: ActorSystem): Unit = {
+
+    }
+
+    def routerAuth(): Unit ={
+
+    }
+}
