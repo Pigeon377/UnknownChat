a21a257c6da69901f10f6a228d7111000d3add1e
pigeon_gugugu <892197240@qq.com>
2022-01-02 00:24:08 +0800

    feat : finish mongo db unique index create

--- src/main/scala/com/thyme/Test.scala
+++ src/main/scala/com/thyme/Test.scala
@@ -1,15 +1,14 @@
 package com.thyme
 
-import akka.util.Timeout
-import org.mongodb.scala.bson.codecs.Macros._
-import org.mongodb.scala.MongoClient.DEFAULT_CODEC_REGISTRY
 import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
-import org.mongodb.scala.{MongoClient, MongoCollection, MongoDatabase}
-import org.mongodb.scala.bson.ObjectId
+import org.bson.types.ObjectId
+import org.mongodb.scala.MongoClient.DEFAULT_CODEC_REGISTRY
+import org.mongodb.scala.bson.codecs.Macros._
+import org.mongodb.scala.model.{IndexOptions, Indexes}
+import org.mongodb.scala.{Document, MongoClient}
 
-import java.util.concurrent.TimeUnit
 import scala.concurrent.Await
-import scala.concurrent.duration.{Duration, DurationInt}
+import scala.concurrent.duration.DurationInt
 import scala.language.postfixOps
 
 object Test {
@@ -17,12 +16,21 @@ object Test {
         val codecRegistry = fromRegistries(fromProviders(classOf[Person]), DEFAULT_CODEC_REGISTRY)
         val mongoClient = MongoClient()
         val database = mongoClient.getDatabase("test").withCodecRegistry(codecRegistry)
-        val collection: MongoCollection[Person] = database.getCollection("thyme")
-        val person = Person("114514", 1919810)
+        val collection = database.getCollection("thyme")
+        Await.result(
+        collection.createIndex(Indexes.ascending("uuid"),IndexOptions().unique(true)).toFuture()
+        ,10 seconds)
+        //        val person = Person("114514", 1919810)
         // !must use sync method ,otherwise cant finish any operation!
-        println(Await.result(collection.insertOne(person).toFuture(), 10 seconds))
-        collection.find().first().map(x => println(x))
+        //        println(Await.result(collection.insertOne(person).toFuture(), 10 seconds))
 
+        Await.result(collection.insertOne(Document(
+            "_id" -> new ObjectId,
+            "uuid"->114514,
+            "name" -> "野兽前辈",
+            "age" -> 1919810
+        )).toFuture(), 7 second)
+        Await.result(collection.find().toFuture(), 7 seconds).foreach(println)
     }
 }
 
