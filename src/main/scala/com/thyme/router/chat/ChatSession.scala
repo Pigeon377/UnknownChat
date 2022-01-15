a9c2a27a4d30767e80f91a8922c1fc007640cb32
pigeon_gugugu <892197240@qq.com>
2022-01-15 23:56:09 +0800

    nil: akka is overload person's brain...

--- src/main/scala/com/thyme/router/chat/ChatSession.scala
+++ src/main/scala/com/thyme/router/chat/ChatSession.scala
@@ -5,6 +5,7 @@ import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.scaladsl.{Flow, Source}
+import spray.json.JsonParser
 
 import scala.concurrent.ExecutionContext
 import scala.language.postfixOps
@@ -19,10 +20,13 @@ object ChatSession {
 
     def websocketFlow: Flow[Message, TextMessage.Strict, NotUsed] ={
         Flow[Message] map {
-            case TextMessage.Strict(text) => TextMessage.Strict(text)
+            case TextMessage.Strict(text) =>
+                JsonParser.apply(text)
+                TextMessage.Strict(text)
         }
     }
 
+    case class WebSocketUser(identifyNumber:String,message:String)
 }
 
 class ChatSession{
