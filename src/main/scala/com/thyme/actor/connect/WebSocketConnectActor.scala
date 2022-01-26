258f6df58abdcd049d096e2cc4bfbb7c737e3fec
pigeon_gugugu <892197240@qq.com>
2022-01-26 23:40:10 +0800

    feat: add broadcast method (websocketFlow) (need refactor)

--- src/main/scala/com/thyme/actor/connect/WebSocketConnectActor.scala
+++ src/main/scala/com/thyme/actor/connect/WebSocketConnectActor.scala
@@ -1,11 +1,34 @@
 package com.thyme.actor.connect
 
-import akka.actor.Actor
+import akka.NotUsed
+import akka.actor.{Actor, ActorRef, ActorSystem}
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
+import akka.stream.{Materializer, OverflowStrategy}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 
-class WebSocketConnectActor extends Actor{
+import java.util.concurrent.Flow.Publisher
+
+class WebSocketConnectActor(implicit system: ActorSystem, mat: Materializer) extends Actor {
     override def receive: Receive = {
         //TODO
         // maintain a websocket connect with user
         ???
+    }
+
+    def websocketFlow(): Flow[Message, TextMessage.Strict,Any] = {
+        val (actor: ActorRef, publisher: Publisher[TextMessage.Strict])=
+            Source.actorRef[String](16,OverflowStrategy.fail)
+                .map(TextMessage.Strict)
+                .toMat(Sink.asPublisher(false))(Keep.both).run()
+
+
+    val sink:Sink[Message,Any] = Flow[Message]
+        .map{
+            case TextMessage.Strict(msg)=>TextMessage.Strict(msg)
+        }
+        .to(Sink.onComplete(
+            null
+        ))
+    Flow.fromSinkAndSource(sink,Source.fromPublisher(publisher))
     }
 }
